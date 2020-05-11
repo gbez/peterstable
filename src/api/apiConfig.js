0370eac4a93d17c4bc2b30f8719769ecc7f87df6
Grant Besner <gmbmonkeyvp@gmail.com>
2020-05-11 12:16:24 -0400

    apiConfig added

---
+++ src/api/apiConfig.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+//Create customized instance of axios
+export default axios.create({
+  baseURL: "http://127.0.0.1:3000/api/v1",
+  headers: {
+    //Get token from Redux store later
+    Authorization: `Bearer token`,
+  },
+});
